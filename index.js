03cf144a87c637bf0b1da584dda97942fedf9457
Akshay Verma <73803832+AkshayDev96@users.noreply.github.com>
2023-06-23 21:39:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ app.use(express.json());
 
 const path = require('path');
 const fs = require('fs').promises;
+const { convertWordFiles } = require('convert-multiple-files-ul')
+// const libre = require('libreoffice-convert');
+// const { spawn } = require('child_process');
+// libre.convertAsync = require('util').promisify(libre.convert);
 
-const libre = require('libreoffice-convert');
-libre.convertAsync = require('util').promisify(libre.convert);
-  
 
 async function main(ext) {
-    const inputPath = "demo"+ext;
+    const inputPath = "demo" + ext;
     const ext_to = ".pdf"
     const outputPath = path.join(__dirname, `/newFile${ext_to}`);
 
@@ -27,52 +28,134 @@ async function main(ext) {
     await fs.writeFile(outputPath, pdfBuf);
 }
 
-function get_url_extension( url ) {
+function get_url_extension(url) {
     return url.split(/[#?]/)[0].split('.').pop().trim();
 }
 
 
-app.post("/gen_pdf", (req, res) => {
+app.post("/gen_word_to_pdf", (req, res) => {
 
-   
-    if(!req?.body?.file_url){
+
+    if (!req?.body?.file_url) {
         return res.send("*Please send file url!");
     }
 
     req.body.file_url = decodeURIComponent(req?.body?.file_url);
 
     const ext = get_url_extension(req?.body?.file_url);
-    
-    if(!ext){
+
+    if (!ext) {
         return res.send("*Please file extension is required!");
     }
 
     const http = require('https'); // or 'https' for https:// URLs
     const fs = require('fs');
 
-    const file = fs.createWriteStream("demo."+ext);
+    const file = fs.createWriteStream("demo." + ext);
     const request = http.get(req.body.file_url, function (response) {
         response.pipe(file);
 
         // after download completed close filestream
-        file.on("finish", () => {
+        file.on("finish", async () => {
             file.close();
-            main("."+ext).then(()=>{
-                res.sendFile(path.join(__dirname, `/newFile.pdf`));
-                // fs.unlink(path.join(__dirname, "/demo."+ext));
+            console.log('demo.' + ext)
+            await convertWordFiles(path.resolve(__dirname, 'demo.' + ext), 'pdf', path.resolve(__dirname));
+            // main("."+ext).then(()=>{
+            res.sendFile(path.join(__dirname, `/demo.pdf`));
+            // fs.unlink(path.join(__dirname, "/demo."+ext));
 
-            }).catch(function (err) {
-                console.log(`Error converting file: ${err}`);
-            })
+            // }).catch(function (err) {
+            //     console.log(`Error converting file: ${err}`);
+            // })
         });
     });
 
 });
 
-app.get("/",(req,res)=>res.send("Welcome to website"));
+app.post("/gen_word_to_any", (req, res) => {
 
-const PORT = 2023;
 
-app.listen(PORT, () => {
-    console.log("Server is running on "+PORT)
+    if (!req?.body?.file_url) {
+        return res.send("*Please send file url!");
+    }
+
+    req.body.file_url = decodeURIComponent(req?.body?.file_url);
+
+    const ext = get_url_extension(req?.body?.file_url);
+
+    if (!ext) {
+        return res.send("*Please file extension is required!");
+    }
+
+    const http = require('https'); // or 'https' for https:// URLs
+    const fs = require('fs');
+
+    const file = fs.createWriteStream("demo." + ext);
+    const request = http.get(req.body.file_url, function (response) {
+        response.pipe(file);
+
+        // after download completed close filestream
+        file.on("finish", async () => {
+            file.close();
+            console.log('demo.' + ext)
+
+            var toPdf = require("office-to-pdf")
+            var wordBuffer = fs.readFileSync("./demo.xlsx")
+
+            toPdf(wordBuffer).then(
+                (pdfBuffer) => {
+                    fs.writeFileSync("./demo.pdf", pdfBuffer)
+                    res.sendFile(path.join(__dirname, `/demo.pdf`));
+                }, (err) => {
+                    console.log(err)
+                }
+            )
+            //  await convertWordFiles(path.resolve(__dirname, 'demo.'+ext), 'pdf', path.resolve(__dirname));
+            // main("."+ext).then(()=>{
+           
+            // fs.unlink(path.join(__dirname, "/demo."+ext));
+
+            // }).catch(function (err) {
+            //     console.log(`Error converting file: ${err}`);
+            // })
+        });
+    });
+
+});
+
+const office = require('node-convert')
+
+
+app.get("/", async (req, res) => {
+    res.send("Welcome to website")
+    try {
+        //   await office.convert('demo.docx', 'test.pdf', ()=>console.log('Done!'));
+        const x = await office.convert(path.join(__dirname, `/demo.docx`), 'xxxxxx.pdf');
+        // const buffer = await office.convert('./demo.docx');
+        console.log(x)
+    } catch (e) {
+        console.log("Error--->", e)
+    }
+});
+
+
+
+// const  test=async()=>{
+//     // Return promise => convertWordFiles(path of the file to be converted, convertTo, outputDir)
+//     const pathOutput = await convertWordFiles(path.resolve(__dirname, 'demo.docx'), 'pdf', path.resolve(__dirname));
+//     console.log(pathOutput);
+//   }
+
+
+
+app.listen(2023,  () => {
+    // try{
+    //     test().then(()=>console.log("done")).catch((e)=>console.log(e))
+    // }catch(e){
+    //     console.log("Error--->",e)
+    // }
+
+    //    console.log(data)
+
+    console.log("Server is running on !!!!!"+2023)
 })
